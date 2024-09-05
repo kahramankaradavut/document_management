da238c6ea6a44a7bdebba9fe8a61ef936782bfb8
Kahraman KARADAVUT <117511123+kahramankaradavut@users.noreply.github.com>
2024-09-05 08:19:16 +0300

    app.js_added

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const apiRoutes = require('./routes/api');
+require('dotenv').config();
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+app.use('/api', apiRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
