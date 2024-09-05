4c6d0bb8db1b16aada9662c794128db954379086
Kahraman KARADAVUT <117511123+kahramankaradavut@users.noreply.github.com>
2024-09-05 08:21:06 +0300

    port_change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/api', apiRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
